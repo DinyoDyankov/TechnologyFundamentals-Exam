80fe990ae404ba1fe51d3c6ebcb79dea5695cf24
Calabria88 <Calabria88@students.softuni.bg>
2019-05-01 11:36:12 +0300

    Add project files.

---
+++ FinalExam/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FinalExam
+{
+    public class Program
+    {
+        static void Main()
+        {
+            string inputCommand = string.Empty;
+
+            Dictionary<string, List<string>> allBandsAndMembers = new Dictionary<string, List<string>>();
+            Dictionary<string, int> allBandsPlayTime = new Dictionary<string, int>();
+
+            while ((inputCommand = Console.ReadLine()) != "start of concert")
+            {
+                string[] currentCommand = inputCommand.Split(new[] { "; " }, StringSplitOptions.RemoveEmptyEntries);
+
+                if (currentCommand[0] == "Add")
+                {
+                    string currentBandToAdd = currentCommand[1];
+                    string[] currentMembersToAdd = currentCommand[2].Split(new[] { ", " }, StringSplitOptions.RemoveEmptyEntries);
+
+                    if (!allBandsAndMembers.ContainsKey(currentBandToAdd) && !allBandsPlayTime.ContainsKey(currentBandToAdd))
+                    {
+                        allBandsAndMembers[currentBandToAdd] = new List<string>();
+                        allBandsPlayTime[currentBandToAdd] = 0;
+
+                        for (int i = 0; i < currentMembersToAdd.Length; i++)
+                        {
+                            if (!allBandsAndMembers[currentBandToAdd].Contains(currentMembersToAdd[i]))
+                            {
+                                allBandsAndMembers[currentBandToAdd].Add(currentMembersToAdd[i]);
+                            }
+                        }
+                    }
+                    else
+                    {
+                        for (int i = 0; i < currentMembersToAdd.Length; i++)
+                        {
+                            if (!allBandsAndMembers[currentBandToAdd].Contains(currentMembersToAdd[i]))
+                            {
+                                allBandsAndMembers[currentBandToAdd].Add(currentMembersToAdd[i]);
+                            }
+                        }
+                    }
+                }
+                else if (currentCommand[0] == "Play")
+                {
+                    string currentBandWithPlayTime = currentCommand[1];
+
+                    int currentBandPlayTime = int.Parse(currentCommand[2]);
+
+                    if (!allBandsAndMembers.ContainsKey(currentBandWithPlayTime) && !allBandsPlayTime.ContainsKey(currentBandWithPlayTime))
+                    {
+                        allBandsAndMembers[currentBandWithPlayTime] = new List<string>();
+                        allBandsPlayTime[currentBandWithPlayTime] = currentBandPlayTime;
+                    }
+                    else
+                    {
+                        allBandsPlayTime[currentBandWithPlayTime] += currentBandPlayTime;
+                    }
+                }
+            }
+
+            string bandToPrintMembers = Console.ReadLine();
+            int totalBandTime = 0;
+
+            foreach (var kvp in allBandsPlayTime)
+            {
+                totalBandTime += kvp.Value;
+            }
+
+            Console.WriteLine($"Total time: {totalBandTime}");
+
+            foreach (var kvp in allBandsPlayTime.OrderByDescending(b => b.Value).ThenBy(b => b.Key))
+            {
+                Console.WriteLine($"{kvp.Key} -> {kvp.Value}");
+            }
+
+            Console.WriteLine(bandToPrintMembers);
+
+            foreach (var kvp in allBandsAndMembers)
+            {
+                if (kvp.Key == bandToPrintMembers)
+                {
+                    foreach (var member in kvp.Value)
+                    {
+                        Console.WriteLine($"=> {member}");
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ P02.SongEncryption/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace P02.SongEncryption
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var regexToValidadeInput = @"^([A-Z][a-z'\s]+):([A-Z \s]+)$";
+
+            string inputLine = string.Empty;
+
+            while ((inputLine = Console.ReadLine()) != "end")
+            {
+                string encryptedMessage = string.Empty;
+
+                if (Regex.IsMatch(inputLine, regexToValidadeInput))
+                {
+                    int keyOfDecryption = 0;
+
+                    var match = Regex.Match(inputLine, regexToValidadeInput);
+
+                    keyOfDecryption = match.Groups[1].Length;
+
+                    string currentMatch = match.ToString();
+
+                    for (int i = 0; i < currentMatch.Length; i++)
+                    {
+                        char currentSymbolsToEncrypt = currentMatch[i];
+
+                        if (char.IsUpper(currentSymbolsToEncrypt))
+                        {
+                            char encryptedCharacter = (char) (currentSymbolsToEncrypt + keyOfDecryption);
+
+                            if (encryptedCharacter > 'Z')
+                            {
+                                int offset = encryptedCharacter - 'Z';
+                                //int remaningValue = keyOfDecryption - ('Z' - currentSymbolsToEncrypt);
+                                //int charToFind = 'A' + remaningValue;
+                                //char charToAdd = (char)charToFind;
+                                encryptedMessage += (char) ('A' + offset - 1);
+                            }
+                            else
+                            {
+                                //int chartofind = currentsymbolstoencrypt + keyofdecryption;
+                                //char chartoadd = (char)chartofind;
+                                encryptedMessage += encryptedCharacter;
+                            }
+                        }
+                        else if (char.IsLower(currentSymbolsToEncrypt))
+                        {
+                            if (currentSymbolsToEncrypt + keyOfDecryption > 122)
+                            {
+                                int remaningValue = keyOfDecryption - (122 - currentSymbolsToEncrypt);
+                                int charToFind = '`' + remaningValue;
+                                char charToAdd = (char)charToFind;
+                                encryptedMessage += charToAdd;
+                            }
+                            else
+                            {
+                                int charToFind = currentSymbolsToEncrypt + keyOfDecryption;
+                                char charToAdd = (char)charToFind;
+                                encryptedMessage += charToAdd;
+                            }
+                        }
+                        else if (currentSymbolsToEncrypt == ':')
+                        {
+                            encryptedMessage += '@';
+                        }
+                        else
+                        {
+                            encryptedMessage += currentSymbolsToEncrypt;
+                        }
+                    }
+
+                    Console.WriteLine($"Successful encryption: {encryptedMessage}");
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input!");
+                }
+            }
+        }
+    }
+}
